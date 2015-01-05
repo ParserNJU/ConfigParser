6650940f2f641b4d5caaeadb12198fcd70089e6a
Daydream <Daydream@172.17.222.54>
2015-01-05 13:06:29 +0800

    打包

--- src/main/java/com/common/Constant.java
+++ src/main/java/com/common/Constant.java
@@ -6,10 +6,11 @@ public class Constant {
 	public static String LOG_NAME = "parser";
 	
 	public static String XML_FOLDER = "/Input_XML";
-	public static String HTML_FOLDER = "/Output_HTML";
+//	public static String HTML_FOLDER = "/Output_HTML";
 	public static String MODEL_FOLDER = "Model_HTML/";
 	public static String FRAMEWORK_FOLDER = "/System/FrameWork";
 	public static String RESOURCE_FOLDER = "/System/ResourceJS";
+	public static String ANDROID_PHONEGAP_FOLDER = "/System/Sample/assets/www/";
 	
 	public static String INIT_HTML_FILE = "initHTMLFile.html";
 	public static String PRE_PROJECT_PATH = "";
